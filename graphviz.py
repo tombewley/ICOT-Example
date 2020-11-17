e089ff4999b435cfe3e83fd2ba463ce5ccc4941c
tombewley <tom.bewley@bristol.ac.uk>
2020-11-17 21:22:51 +0000

    Graphviz -> pydot -> networkx

--- graphviz.py
+++ graphviz.py
@@ -1,5 +1,16 @@
 import pydot 
+import networkx
+import matplotlib.pyplot as plt
 
-with open('src/tree.dot', 'r') as f: graph = f.read()
-(graph,) = pydot.graph_from_dot_data(graph) 
-graph.write_png('tree.png') 
+# Open with pydot.
+with open('src/tree.dot', 'r') as f: graph_spec = f.read()
+(graph_dot,) = pydot.graph_from_dot_data(graph_spec)
+graph_dot.write_png('tree.png') 
+
+# Convert to networkx
+graph = networkx.drawing.nx_pydot.from_pydot(graph_dot)
+# for node in graph.nodes:
+#     print(graph.nodes[node])
+pos = networkx.drawing.nx_pydot.graphviz_layout(graph)
+networkx.draw(graph, pos)
+plt.show()
